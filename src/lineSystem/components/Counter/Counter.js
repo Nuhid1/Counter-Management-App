60744c8b07ef3615b32089c9b4023c47b46f80eb
Nuhid1 <nuhid96@Gmail.com>
2025-09-15 14:32:55 +0600

    Added delete counter toast feature

--- src/lineSystem/components/Counter/Counter.js
+++ src/lineSystem/components/Counter/Counter.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { ToastContainer, toast } from 'react-toastify';
 import style from './counter.module.css'
 
 const Counter = (props) => {
@@ -8,6 +9,7 @@ const Counter = (props) => {
 
     const handleDelete = ()=>{
       props.deleteCounter(id);
+      toast(`Counter (${name}) Deleted`);
     }
   return (
      <div key={id} className={style["counters-card"]}>
